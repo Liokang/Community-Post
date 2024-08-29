8980770df95201a972f092225f24f74388fef242
unknown <atul2710rawat@gmail.com>
2024-08-29 21:42:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Routes
+const routes = require('./routes/index');
+app.use('/', routes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`listening on port:${port}`);
+});
